0f988a27519187422a648fdf40eaae9cbdc33a0f
marcoconvergia <marcoconvergia@gmail.com>
2018-10-17 22:04:14 -0400

    php system update table center

--- dashboard.php
+++ dashboard.php
@@ -69,25 +69,7 @@
 				require "received.php";	
 			}	
 		?>
-          
-          <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">IP </th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Estado</th>
-      <th scope="col">Latencia</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">100.00.00.00</th>
-      <td>Example1</td>
-      <td>Verde</td>
-      <td>100</td>
-    </tr>
-      </tbody>
-</table>  
+         
         
         
         </div>
